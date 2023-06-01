1571030e956d3aa02835700d66d12ab2c272da12
AdrianRotarasu <72971256+AdrianRotarasu@users.noreply.github.com>
2023-06-01 23:26:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from flask import Flask, jsonify
-import os
-
+from flask import Flask, render_template, request, send_from_directory
+import os 
+from deeplearning import modify
+from flask_cors import CORS
+# webserver gateway interface
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-@app.route('/')
+BASE_PATH = os.getcwd()
+UPLOAD_PATH = os.path.join(BASE_PATH,'static/upload/')
+
+
+@app.route('/',methods=['POST','GET'])
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    if request.method == 'POST':
+        upload_file = request.files['image']
+        filename = upload_file.filename
+        path_save = os.path.join(UPLOAD_PATH,filename)
+        upload_file.save(path_save)
+        text = modify(path_save,filename)
+        print(len(text))
+        if len(text) <2 :
+            text="No text found"
+        # return render_template('index.html', upload=True, upload_image=filename ,text=text)
+        return filename
+
+    # return render_template('index.html', upload=False)
+
+@app.route('/uploads/<filename>', methods=['GET'])
+def get_uploaded_file(filename):
+    return send_from_directory('static/modified_uploads', filename)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+if __name__ =="__main__":
+    app.run(debug=True)
