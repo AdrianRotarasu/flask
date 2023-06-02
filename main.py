b882a4a2cf92e1abe2f1e609639122a6f315d04f
AdrianRotarasu <72971256+AdrianRotarasu@users.noreply.github.com>
2023-06-02 12:06:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request
 import os 
+import base64
+from PIL import Image
+from io import BytesIO
 from deeplearning import modify
-from flask_cors import CORS
 # webserver gateway interface
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
-
 
 BASE_PATH = os.getcwd()
 UPLOAD_PATH = os.path.join(BASE_PATH,'static/upload/')
@@ -14,24 +14,25 @@ UPLOAD_PATH = os.path.join(BASE_PATH,'static/upload/')
 @app.route('/',methods=['POST','GET'])
 def index():
     if request.method == 'POST':
-        upload_file = request.files['image']
+        upload_file = request.files['image_name']
         filename = upload_file.filename
-        path_save = os.path.join(UPLOAD_PATH,filename)
-        upload_file.save(path_save)
-        text = modify(path_save,filename)
-        print(len(text))
-        if len(text) <2 :
-            text="No text found"
-        # return render_template('index.html', upload=True, upload_image=filename ,text=text)
-        return filename
+        modified_image = modify(upload_file)
+       # Convert the uploaded image to base64 string
+        upload= image = Image.open(upload_file)
+        buffered = BytesIO()
+        upload.save(buffered, format='PNG')  # Save as PNG format
+        upload_image_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        
+        # Convert the modified image to base64 string
+        buffered = BytesIO()
+        modified_image.save(buffered, format='PNG')  # Save as PNG format
+        modified_image_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
-    # return render_template('index.html', upload=False)
+        return render_template('index.html', upload=True, upload_image=upload_image_base64, modified=modified_image_base64)
+    return render_template('index.html', upload=False)
 
-@app.route('/uploads', methods=['GET'])
-#def get_uploaded_file(filename):
-def get_uploaded_file():
-    #return send_from_directory('static/modified_uploads', filename)
-    return get_uploaded_file('static/modified_uploads/adi6.jpg')
+
+
 
 if __name__ =="__main__":
     app.run(debug=True)
