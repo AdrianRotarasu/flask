4fe559cf74ba1856dd4efa8c4533a56462b1eaac
AdrianRotarasu <72971256+AdrianRotarasu@users.noreply.github.com>
2023-06-02 12:06:59 +0300

    Update deeplearning.py

--- deeplearning.py
+++ deeplearning.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import pytesseract as pt
 from PIL import Image, ImageFilter
 from ultralytics import YOLO
-
+import base64
+from io import BytesIO
 
 #rf = Roboflow(api_key="Bv81WfXp2nY9ZIi2UdH6")
 #project = rf.workspace().project("vehicle-registartion-plate-finder")
@@ -15,12 +16,11 @@ modelYolov8 = YOLO('./yolov8v2.pt')
 
 
 
-def modify(path, filename):
-    results = modelYolov8(path)
+def modify(file_stream):
+    image = Image.open(file_stream)
+    results = modelYolov8(image)
     cods = results[0].boxes.xyxy
-    img = np.array(Image.open(path))
-    
-    image = Image.open(path)
+    img = np.array(image)
     image.load()
     
     for cord in cods:
@@ -28,14 +28,6 @@ def modify(path, filename):
         
         crop = image.crop((xmin, ymin, xmax, ymax))
         image.paste(crop.filter(ImageFilter.GaussianBlur(20)), (xmin, ymin))
-  
-        #roi = img[ymin:ymax, xmin:xmax]
-        #roi_bgr = cv2.cvtColor(roi, cv2.COLOR_RGB2BGR)
-        # cv2.imwrite('./static/roi/{}'.format(filename), roi_bgr)
-    image = image.save('./static/modified_uploads/{}'.format(filename))
-    text="abc"
-    #text = pt.image_to_string(roi)
-    #print(text)
-    return text
 
+    return image
 
